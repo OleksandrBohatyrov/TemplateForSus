31d3a29ef3e7e2f4435d6ba9a370737a88d9d4b3
OleksandrBohatyrov <alexboggg361@gmail.com>
2024-09-05 23:03:04 +0300

    для суса

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TemplateForSus.Startup))]
+namespace TemplateForSus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
